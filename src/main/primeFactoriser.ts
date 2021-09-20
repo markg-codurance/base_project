1935df36bc65c2ffaf048c5dc986012b2ef6aacc
Mark Gray <mark.gray@codurance.com>
2021-09-20 15:46:00 +0100

    White space refactor

--- src/main/primeFactoriser.ts
+++ src/main/primeFactoriser.ts
@@ -1,20 +1,16 @@
 export function primeFactoriser(input: number) : number[] {
-
     let result: number[] = [];
 
     let divisor = 2;
     
     while (input > 1) {
-
         while (input % divisor === 0) {
             result.push(divisor);
             input /= divisor;
         }
 
         divisor++;
-
     }
 
     return result;
-
 }
